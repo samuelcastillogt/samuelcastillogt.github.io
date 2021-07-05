ebe782fd74e41a3004c1985ce92d3fcbab814448
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2021-07-05 16:33:25 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-$(".selector-menu").click(()=>{
-    $(".menu-responsive").toggle("hidden")
-    
-    
-})
+function saludar(nombre){
+    const nombre = nombre
+    const saludo = "Hola como estas"
+    const completo = saludo + " " + nombre
+    return completo
+}
+saludar("jorge")
+console.log(completo)
