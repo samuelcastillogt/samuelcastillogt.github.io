21dbc30d03a2d5e9be15949424437ff4ea16f610
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2021-11-02 20:47:08 -0600

    Agrege Spa de Rick and Morty

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function saludar(nombre){
     const nombre = nombre
     const saludo = "Hola como estas"
@@ -5,4 +6,13 @@ function saludar(nombre){
     return completo
 }
 saludar("jorge")
+=======
+function saludar(nombre){
+    const nombre = nombre
+    const saludo = "Hola como estas"
+    const completo = saludo + " " + nombre
+    return completo
+}
+saludar("jorge")
+>>>>>>> 6bcb0ac847d9a7fa2574acb5ee6f7d04be5d2ae3
 console.log(completo)
